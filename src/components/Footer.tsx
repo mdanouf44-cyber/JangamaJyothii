876590b5e3f4bebffea4eea31451f193e8df5744
mdanouf44-cyber <mdanouf44.cyber@gmail.com>
2026-01-31 10:56:46 +0530

    Add LinkedIn URL to footer social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -70,7 +70,9 @@ const Footer = () => {
                 <Facebook className="w-5 h-5" />
               </a>
               <a
-                href="#"
+                href="https://www.linkedin.com/in/shree-jangama-jyothi-global-llp-461285381/"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center hover:bg-green-600 transition-colors duration-300"
               >
                 <Linkedin className="w-5 h-5" />
